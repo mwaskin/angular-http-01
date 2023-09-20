6817aa662833e7ced77acb4be9f7c90aec6fa0b0
Matthew Waskin <matthew@waskin.com>
2023-09-20 16:36:47 -0400

    use map operator to transform the fetchPosts response into an array of posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -39,6 +40,17 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http
       .get('https://ng-http-01-bcdb6-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map((responseData) => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
       .subscribe((posts) => {
         console.log(posts);
       });
